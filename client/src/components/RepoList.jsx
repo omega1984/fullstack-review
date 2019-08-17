911018efa48338c46d0139e2f60ec68f3eb72265
omega1984 <omegareborn2019@gmail.com>
2019-08-16 23:23:18 -0700

    working on top 25 without refresh the page

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -3,7 +3,25 @@ import React from 'react';
 const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
+    {
+      props.repos.map((repo, index) => {
+      if (index === 0){
+        return repo.login
+      }
+    })
+    }' repos:
+
+    {
+      props.repos.map((repo, index) =>{
+        if (index < 25){
+          return <div>{index+1}: <a href={`${repo.url}`}>{repo.url}</a></div>
+        }
+      })
+    }
+
+    <div>
+    There are total {props.repos.length} repos.
+    </div>
   </div>
 )
 
